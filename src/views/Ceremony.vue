a2639ade80aa00849cf16793f6f9174a27818c8e
李富荣 <lifurong1@cloudwalk.com>
2020-10-09 18:19:28 +0800

    update ceremony

--- src/views/Ceremony.vue
+++ src/views/Ceremony.vue
@@ -2,18 +2,23 @@
   <v-container>
     <v-stepper v-model="step">
       <v-stepper-header>
-        <v-stepper-step step="1" :complete="step > 1">
+        <v-stepper-step step="1" :complete="step > 1" editable>
           Choose Plans
         </v-stepper-step>
         <v-divider></v-divider>
-        <v-stepper-step step="2" :complete="step > 2">
+        <v-stepper-step step="2" :complete="step > 2" editable>
           Comfirm Items
         </v-stepper-step>
         <v-divider></v-divider>
-        <v-stepper-step step="2" :complete="step > 2">
+        <v-stepper-step step="3" :complete="step > 3" editable>
           Admit Master
         </v-stepper-step>
       </v-stepper-header>
+      <v-stepper-items>
+        <v-stepper-content step="1">choose your plan</v-stepper-content>
+        <v-stepper-content step="2">Confirm your items</v-stepper-content>
+        <v-stepper-content step="3">Admit your Mast</v-stepper-content>
+      </v-stepper-items>
     </v-stepper>
   </v-container>
 </template>

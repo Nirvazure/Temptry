a31332ac1bdddb7f43f4747a51a7223ed96e436b
李富荣 <lifurong1@cloudwalk.com>
2020-10-10 17:57:45 +0800

    add aplayer

--- src/views/Plan.vue
+++ src/views/Plan.vue
@@ -1,20 +1,41 @@
 <template>
   <v-container>
     <h2>Plan</h2>
-    <v-card>
-      <v-list shaped>
-        <v-list-item-group>
-          <draggable v-model="items">
-            <v-list-item v-for="(item, i) in items" :key="i">
-              <v-list-item-content>
-                <v-list-item-title>{{ item }}</v-list-item-title>
-                <v-list-item-subtitle>sdas</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </draggable>
-        </v-list-item-group>
-      </v-list>
-    </v-card>
+
+    <v-row>
+      <v-col>
+        <v-card color="primary" dark>
+          <v-card-title>Items</v-card-title>
+          <v-card-text>
+            <v-list>
+              <v-list-item-group>
+                <draggable v-model="items" group="a1">
+                  <v-list-item v-for="(item, i) in items" :key="i">
+                    {{ item }}{{ i }}
+                  </v-list-item>
+                </draggable>
+              </v-list-item-group>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card color="success" dark>
+          <v-card-title>choices</v-card-title>
+          <v-card-text>
+            <v-list>
+              <v-list-item-group>
+                <draggable v-model="choices" group="a1">
+                  <v-list-item v-for="(item, i) in choices" :key="i">
+                    {{ item }}{{ i }}
+                  </v-list-item>
+                </draggable>
+              </v-list-item-group>
+            </v-list>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -23,7 +44,8 @@ import draggable from "vuedraggable";
 export default {
   components: { draggable },
   data: () => ({
-    items: ["TOP", "JUG", "MID", "BOT", "SUP"],
+    items: ["JUG", "MID", "BOT", "SUP"],
+    choices: ["TOP"],
   }),
 };
 </script>
--- src/views/Train.vue
+++ src/views/Train.vue
@@ -2,16 +2,29 @@
   <v-container>
     <h2>Promise Try</h2>
     <v-btn color="primary">Start</v-btn>
+    <aplayer
+      autoplay
+      :music="{
+        title: 'secret base~君がくれたもの~',
+        artist: 'Silent Siren',
+        src: 'http://sc1.111ttt.cn/2018/1/03/13/396131232171.mp3',
+        pic: 'https://moeplayer.b0.upaiyun.com/aplayer/secretbase.jpg',
+      }"
+    />
   </v-container>
 </template>
 
 <script>
+import Aplayer from "vue-aplayer";
 const promise = new Promise((resolve, reject) => {
   setTimeout(resolve, 3000, "done");
   reject;
 });
 
 export default {
+  components: {
+    Aplayer,
+  },
   data: () => ({}),
   mounted() {
     promise.then((v) => {

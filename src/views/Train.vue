b49ff3656ee8a6a6df05249f3657f2d0caf853af
李富荣 <lifurong1@cloudwalk.com>
2020-10-23 17:33:20 +0800

    update

--- src/views/Train.vue
+++ src/views/Train.vue
@@ -1,41 +1,74 @@
 <template>
-  <v-container class="pa-12">
-    <v-row>
-      <v-col cols="12" md="2" class="d-flex justify-space-around">
-        <v-img :src="img" :aspect-ratio="3 / 4" width="100%"></v-img>
+  <v-container>
+    <v-row dense>
+      <v-col cols="12">
+        <v-card color="#385F73" dark>
+          <v-card-title class="headline"> Unlimited music now </v-card-title>
+
+          <v-card-subtitle
+            >Listen to your favorite artists and albums whenever and wherever,
+            online and offline.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn text> Listen Now </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
-      <v-col cols="12" md="3" class="ml-10">
-        <v-row>
-          <p class="display-1 mt-3">Celine Kutch</p>
-        </v-row>
-        <v-row>
-          <v-col cols="12" md="4">
-            <p class="font-weight-bold">Item1</p>
-            <p class="font-weight-bold">Item1</p>
-            <p class="font-weight-bold">Item1</p>
-            <p class="font-weight-bold">Item1</p>
-          </v-col>
-          <v-col cols="12" md="7">
-            <p>Item1</p>
-            <p>Item1</p>
-            <p>Item1</p>
-            <p>Item1</p>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-btn outlined color="primary" class="mx-1">Order</v-btn>
-          <v-btn outlined color="success" class="mx-1">Order</v-btn>
-          <v-btn outlined color="warning" class="mx-1">Order</v-btn>
-        </v-row>
+
+      <v-col v-for="(item, i) in items" :key="i" cols="12">
+        <v-card :color="item.color" dark>
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title class="headline" v-text="item.title"></v-card-title>
+
+              <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+              <v-card-actions>
+                <v-btn
+                  v-if="item.artist === 'Ellie Goulding'"
+                  class="ml-2 mt-3"
+                  fab
+                  icon
+                  height="40px"
+                  right
+                  width="40px"
+                >
+                  <v-icon>mdi-play</v-icon>
+                </v-btn>
+
+                <v-btn v-else class="ml-2 mt-5" outlined rounded small>
+                  START RADIO
+                </v-btn>
+              </v-card-actions>
+            </div>
+
+            <v-avatar class="ma-3" size="125" tile>
+              <v-img :src="item.src"></v-img>
+            </v-avatar>
+          </div>
+        </v-card>
       </v-col>
-      <v-col cols="12" md="6"></v-col>
     </v-row>
   </v-container>
-</template>
-<script>
+</template><script>
 export default {
   data: () => ({
     img: require("@/assets/1.jpg"),
+    items: [
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#952175",
+        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
+        title: "Halcyon Days",
+        artist: "Ellie Goulding",
+      },
+    ],
   }),
 };
 </script>
